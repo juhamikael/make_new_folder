cbc48d9b0022c454088d9e69d3483137d1e4611f
Juha Savikko <juha.savikko@tuni.fi>
2021-10-31 01:48:00 +0300

    Cleaned code repeat

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import os
 import shutil
 
-
 # inpt = input("path:")  # Folder to "go through"
 inpt = "D:/53 September 2021"
 path = inpt
-
-
-# def check_ending(dir_names, files_dict ):
-#     dirs = dir_names
-
 
 file_types = {
     # File types which are used in for loop in line 51-72
@@ -29,43 +23,11 @@ file_types = {
     # Make new "folder here"
     "Misc": []
 }
-folder_names = (
-    # Folder names to match 'file_types'
-    "Documents",
-    "Images",
-    "Audio",
-    "Installers",
-    "Packed Files",
-    "Music Production",
-    "Midi",
-    "Video",
-    "Excel",
-    "Sound Banks & Presets",
-    "Photoshop",
-    "Torrent",
-    # Make new "folder here
-    "PDF",
-    "Misc"
-)
-print(type(folder_names))
-file_map = {
+folder_names = tuple([key for key in file_types.keys()])
+file_map = {}
+for new_key in folder_names:
+    file_map[new_key] = []
 
-    "Documents": [],
-    "Images": [],
-    "Audio": [],
-    "Installers": [],
-    "Packed Files": [],
-    "Music Production": [],
-    "Midi": [],
-    "Video": [],
-    "Excel": [],
-    "Sound Banks & Presets": [],
-    "Photoshop": [],
-    "Torrent": [],
-    # Make new "folder here"
-    "PDF": [],
-    "Misc": []
-}
 file_or_folder_map = {
     "Files": [],
     "Folders": []
@@ -78,50 +40,18 @@ for item in listdir:
     elif os.path.isdir(f"{path}/{item}"):
         file_or_folder_map["Folders"].append(item)
 
-
 for item in listdir:
     if os.path.isfile(f"{path}/{item}"):
         file_or_folder_map["Files"].append(item)
     elif os.path.isdir(f"{path}/{item}"):
         file_or_folder_map["Folders"].append(item)
 
-
-
-
-
-
-
 for file in file_or_folder_map["Files"]:
-    # Checking files in the path folder
-    # If file ends with .pdf, .docx, .rtf etc, adding it to filemap documents, check lines 7-18
-    if file.endswith(tuple(file_types['Documents'])):
-        file_map["Documents"].append(file)
-    elif file.endswith(tuple(file_types['Images'])):
-        file_map["Images"].append(file)
-    elif file.endswith(tuple(file_types['Audio'])):
-        file_map["Audio"].append(file)
-    elif file.endswith(tuple(file_types['Installers'])):
-        file_map["Installers"].append(file)
-    elif file.endswith(tuple(file_types['Packed Files'])):
-        file_map["Packed Files"].append(file)
-    elif file.endswith(tuple(file_types['Music Production'])):
-        file_map["Music Production"].append(file)
-    elif file.endswith(tuple(file_types['Midi'])):
-        file_map["Midi"].append(file)
-    elif file.endswith(tuple(file_types['Video'])):
-        file_map["Video"].append(file)
-    elif file.endswith(tuple(file_types['Excel'])):
-        file_map["Excel"].append(file)
-    elif file.endswith(tuple(file_types['Sound Banks & Presets'])):
-        file_map["Sound Banks & Presets"].append(file)
-    elif file.endswith(tuple(file_types['Photoshop'])):
-        file_map["Photoshop"].append(file)
-    elif file.endswith(tuple(file_types['Torrent'])):
-        file_map["Torrent"].append(file)
-    elif file.endswith(tuple(file_types['PDF'])):
-        file_map["PDF"].append(file)
-    else:
-        file_map["Misc"].append(file)
+    for folder in folder_names:
+        if file.endswith(tuple(file_types[f'{folder}'])):
+            file_map[f"{folder}"].append(file)
+        else:
+            file_map["Misc"].append(file)
 
 for folder_name in folder_names:
     files_for_folder = file_map[folder_name]
