e1371b1ab8b7b950c03f464808ec49adb4dffaf2
juhamikael <83360104+juhamikael@users.noreply.github.com>
2021-11-14 17:09:14 +0200

    Delete functions.py

--- functions.py
+++
@@ -1,9 +0,0 @@
-def check_ending(dir_names, file_map, files_dict, endings):
-
-    files = files_dict
-    for file in files:
-        for ending in endings:
-            for folder_name in dir_names:
-                if file.endswith(tuple(ending[folder_name])):
-                    file_map[folder_name].append(file)
-    return file_map
